0e175350a755e6497c4d136ea82fd251dd774d64
Serhii Chernenko <serhii.chernenko@vaimo.com>
2024-08-13 14:21:05 +0200

    repo prepared

---
+++ tests/vTabs-vTabPanel.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import vTabs from '../src/components/vTabs.vue'
+import vTabPanel from '../src/components/vTabPanel.vue'
+
+describe('the use of vTabsPanel with vTabs', () => {
+    it.todo('renders the tab titles', async () => {})
+
+    it.todo('renders the tab panel content', async () => {})
+
+    it.todo('only shows the content for the active panel', async () => {})
+
+    it.todo('switches the content based on the tab clicked', async () => {})
+})
